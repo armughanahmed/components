2cf011db09dd7e1b424c702d5c5257fea5372d9a
Armughan Ahmed <armughan.ahmed@yahoo.com>
2020-08-08 23:24:59 +0500

    children component added

--- src/index.js
+++ src/index.js
@@ -2,44 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import Approve from "./Approve";
 const App = () => {
   return (
     <div class="ui comments">
-      <CommentDetail
-        author={faker.name.findName()}
-        time={faker.date.weekday()}
-        star={faker.random.number()}
-        text={faker.lorem.text()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.findName()}
-        time={faker.date.weekday()}
-        star={faker.random.number()}
-        text={faker.lorem.text()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.findName()}
-        time={faker.date.weekday()}
-        star={faker.random.number()}
-        text={faker.lorem.text()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.findName()}
-        time={faker.date.weekday()}
-        star={faker.random.number()}
-        text={faker.lorem.text()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.name.findName()}
-        time={faker.date.weekday()}
-        star={faker.random.number()}
-        text={faker.lorem.text()}
-        avatar={faker.image.avatar()}
-      />
+      <Approve>
+        <CommentDetail
+          author={faker.name.findName()}
+          time={faker.date.weekday()}
+          star={faker.random.number()}
+          text={faker.lorem.text()}
+          avatar={faker.image.avatar()}
+        />
+      </Approve>
+      <Approve>
+        <CommentDetail
+          author={faker.name.findName()}
+          time={faker.date.weekday()}
+          star={faker.random.number()}
+          text={faker.lorem.text()}
+          avatar={faker.image.avatar()}
+        />
+      </Approve>
+      <Approve>
+        <CommentDetail
+          author={faker.name.findName()}
+          time={faker.date.weekday()}
+          star={faker.random.number()}
+          text={faker.lorem.text()}
+          avatar={faker.image.avatar()}
+        />
+      </Approve>
+      <Approve>
+        <CommentDetail
+          author={faker.name.findName()}
+          time={faker.date.weekday()}
+          star={faker.random.number()}
+          text={faker.lorem.text()}
+          avatar={faker.image.avatar()}
+        />
+      </Approve>
+      <Approve>
+        <CommentDetail
+          author={faker.name.findName()}
+          time={faker.date.weekday()}
+          star={faker.random.number()}
+          text={faker.lorem.text()}
+          avatar={faker.image.avatar()}
+        />
+      </Approve>
     </div>
   );
 };
